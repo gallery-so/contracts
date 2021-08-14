01233ddf4447213b83b5f386aab6f034c7238c92
bennycio <benny@bennyc.io>
2021-08-14 11:26:01 -0700

    bit wizardry for NF/F ERC1155 tokens

---
+++ contracts/ERC1155MixedFungible.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+import "./ERC1155.sol";
+
+/**
+ * @dev Enjin implementation of the MixedFungible Token.
+ * See https://eips.ethereum.org/EIPS/eip-1155
+ * Originally based on code by Enjin: https://github.com/enjin/erc-1155
+ * Adapted by Benny Conn
+ */
+contract ERC1155MixedFungible is ERC1155Upgradeable {
+    // Use a split bit implementation.
+    // Store the type in the upper 128 bits..
+    uint256 constant TYPE_MASK = uint256(type(uint128).max) << 128;
+
+    // ..and the non-fungible index in the lower 128
+    uint256 constant NF_INDEX_MASK = type(uint128).max;
+
+    // The top bit is a flag to tell if this is a NFI.
+    uint256 constant TYPE_NF_BIT = 1 << 255;
+
+    mapping(uint256 => address) nfOwners;
+
+    function isNonFungible(uint256 _id) public pure returns (bool) {
+        return _id & TYPE_NF_BIT == TYPE_NF_BIT;
+    }
+
+    function isFungible(uint256 _id) public pure returns (bool) {
+        return _id & TYPE_NF_BIT == 0;
+    }
+
+    function getNonFungibleIndex(uint256 _id) public pure returns (uint256) {
+        return _id & NF_INDEX_MASK;
+    }
+
+    function getNonFungibleBaseType(uint256 _id) public pure returns (uint256) {
+        return _id & TYPE_MASK;
+    }
+
+    function isNonFungibleBaseType(uint256 _id) public pure returns (bool) {
+        // A base type has the NF bit but does not have an index.
+        return (_id & TYPE_NF_BIT == TYPE_NF_BIT) && (_id & NF_INDEX_MASK == 0);
+    }
+
+    function isNonFungibleItem(uint256 _id) public pure returns (bool) {
+        // A base type has the NF bit but does has an index.
+        return (_id & TYPE_NF_BIT == TYPE_NF_BIT) && (_id & NF_INDEX_MASK != 0);
+    }
+
+    function ownerOf(uint256 _id) public view returns (address) {
+        return nfOwners[_id];
+    }
+
+    // override
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _id,
+        uint256 _value,
+        bytes calldata _data
+    ) external virtual override {
+        require(_to != address(0x0), "cannot send to zero address");
+        require(
+            _from == msg.sender || operatorApprovals[_from][msg.sender] == true,
+            "Need operator approval for 3rd party transfers."
+        );
+
+        if (isNonFungible(_id)) {
+            require(nfOwners[_id] == _from);
+            nfOwners[_id] = _to;
+        } else {
+            balances[_id][_from] -= _value;
+            balances[_id][_to] += _value;
+        }
+
+        emit TransferSingle(msg.sender, _from, _to, _id, _value);
+
+        _doSafeTransferAcceptanceCheck(
+            msg.sender,
+            _from,
+            _to,
+            _id,
+            _value,
+            _data
+        );
+    }
+
+    function safeBatchTransferFrom(
+        address _from,
+        address _to,
+        uint256[] calldata _ids,
+        uint256[] calldata _values,
+        bytes calldata _data
+    ) external virtual override {
+        require(_to != address(0x0), "cannot send to zero address");
+        require(_ids.length == _values.length, "Array length must match");
+
+        // Only supporting a global operator approval allows us to do only 1 check and not to touch storage to handle allowances.
+        require(
+            _from == msg.sender || operatorApprovals[_from][msg.sender] == true,
+            "Need operator approval for 3rd party transfers."
+        );
+
+        for (uint256 i = 0; i < _ids.length; ++i) {
+            // Cache value to local variable to reduce read costs.
+            uint256 id = _ids[i];
+            uint256 value = _values[i];
+
+            if (isNonFungible(id)) {
+                require(nfOwners[id] == _from);
+                nfOwners[id] = _to;
+            } else {
+                balances[id][_from] -= value;
+                balances[id][_to] += value;
+            }
+        }
+
+        emit TransferBatch(msg.sender, _from, _to, _ids, _values);
+
+        _doSafeBatchTransferAcceptanceCheck(
+            msg.sender,
+            _from,
+            _to,
+            _ids,
+            _values,
+            _data
+        );
+    }
+
+    function balanceOf(address _owner, uint256 _id)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        if (isNonFungibleItem(_id)) return nfOwners[_id] == _owner ? 1 : 0;
+        return balances[_id][_owner];
+    }
+
+    function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids)
+        external
+        view
+        override
+        returns (uint256[] memory)
+    {
+        require(_owners.length == _ids.length);
+
+        uint256[] memory balances_ = new uint256[](_owners.length);
+
+        for (uint256 i = 0; i < _owners.length; ++i) {
+            uint256 id = _ids[i];
+            if (isNonFungibleItem(id)) {
+                balances_[i] = nfOwners[id] == _owners[i] ? 1 : 0;
+            } else {
+                balances_[i] = balances[id][_owners[i]];
+            }
+        }
+
+        return balances_;
+    }
+}
--- contracts/Invite1155.sol
+++ contracts/Invite1155.sol
@@ -2,68 +2,111 @@
 
 pragma solidity ^0.8.6;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "./ERC1155MixedFungible.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./Redeemable.sol";
 
-contract Invite1155 is
-    Initializable,
-    AccessControlUpgradeable,
-    ERC1155Upgradeable
-{
+/**
+ * @dev Based on Enjin implementation of the MixedFungibleMintable Token.
+ * https://github.com/enjin/erc-1155
+ * Adapted by Benny Conn
+ */
+contract Invite1155 is Initializable, ERC1155MixedFungible, Redeemable {
+    uint256 nonce;
+    mapping(uint256 => address) public creators;
+    mapping(uint256 => uint256) public maxIndex;
+
+    modifier creatorOnly(uint256 _id) {
+        require(creators[_id] == msg.sender);
+        _;
+    }
+
+    // This function only creates the type.
+    function create(bool _isNF) external returns (uint256 _type) {
+        // Store the type in the upper 128 bits
+        _type = (++nonce << 128);
+
+        // Set a flag if this is an NFI.
+        if (_isNF) _type = _type | TYPE_NF_BIT;
+
+        // This will allow restricted access to creators.
+        creators[_type] = msg.sender;
+
+        // emit a Transfer event with Create semantic to help with discovery.
+        emit TransferSingle(msg.sender, address(0x0), address(0x0), _type, 0);
+    }
+
     function initialize(string memory baseTokenURI) public virtual initializer {
         __invite_init(baseTokenURI);
     }
 
     function __invite_init(string memory baseTokenURI) internal initializer {
         __ERC165_init_unchained();
-        __AccessControl_init_unchained();
         __ERC1155_init_unchained(baseTokenURI);
         __Context_init_unchained();
         __invite_init_unchained();
     }
 
-    function __invite_init_unchained() internal initializer {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    }
+    function __invite_init_unchained() internal initializer {}
 
-    function mint(
-        address to,
-        uint256 tokenId,
-        uint256 amount
-    ) public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
-        _mint(to, tokenId, amount, abi.encodePacked(msg.sender));
-    }
+    function mintNonFungible(uint256 _type, address[] calldata _to)
+        external
+        creatorOnly(_type)
+    {
+        require(isNonFungible(_type));
 
-    function mintBatch(
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts
-    ) public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
-        _mintBatch(to, ids, amounts, abi.encodePacked(msg.sender));
-    }
+        // Index are 1-based.
+        uint256 index = maxIndex[_type] + 1;
+        maxIndex[_type] += _to.length;
 
-    function mintToMany(
-        address[] memory tos,
-        uint256[] memory ids,
-        uint256[] memory amounts
-    ) public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(
-            tos.length == ids.length && tos.length == amounts.length,
-            "Invite: Mismatched array sizes"
-        );
-        for (uint256 i = 0; i < tos.length; i++) {
-            _mint(tos[i], ids[i], amounts[i], abi.encodePacked(msg.sender));
+        for (uint256 i = 0; i < _to.length; ++i) {
+            address dst = _to[i];
+            uint256 id = _type | (index + i);
+
+            nfOwners[id] = dst;
+
+            emit TransferSingle(msg.sender, address(0x0), dst, id, 1);
+
+            _doSafeTransferAcceptanceCheck(
+                msg.sender,
+                msg.sender,
+                dst,
+                id,
+                1,
+                ""
+            );
         }
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(AccessControlUpgradeable, ERC1155Upgradeable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    function mintFungible(
+        uint256 _id,
+        address[] calldata _to,
+        uint256[] calldata _quantities
+    ) external creatorOnly(_id) {
+        require(isFungible(_id));
+
+        for (uint256 i = 0; i < _to.length; ++i) {
+            address to = _to[i];
+            uint256 quantity = _quantities[i];
+
+            // Grant the items to the caller
+            balances[_id][to] += quantity;
+
+            emit TransferSingle(msg.sender, address(0x0), to, _id, quantity);
+
+            _doSafeTransferAcceptanceCheck(
+                msg.sender,
+                msg.sender,
+                to,
+                _id,
+                quantity,
+                ""
+            );
+        }
+    }
+
+    function redeem(uint256 _id) public virtual override {
+        require(isNonFungible(_id), "Invite: token must be NF to be redeemed");
+        super.redeem(_id);
     }
 }
