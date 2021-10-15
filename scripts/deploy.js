926058ac1b04dece985092bcfe4058fce1345957
bennycio <benny@bennyc.io>
2021-10-15 19:28:36 -0400

    new lines

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,7 @@
 async function main() {
   const Contract = await ethers.getContractFactory("Invite1155")
   const contract = await Contract.deploy(
-    `ipfs://QmUUyAu9Uz5jaiEwLgm6wjhMsAMd1kvTbzb8K18184RKHK/`,
+    `ipfs://QmQFzJPZMFU6SPYLhSL4HSnjEc7FCocaf5tW2GtZAv2Hy2/`,
     "Gallery Membership Cards",
     "GMC"
   )
