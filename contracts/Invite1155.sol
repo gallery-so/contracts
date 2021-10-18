7716e261fda43e81e47de6b9383c600130577e15
bennycio <benny@bennyc.io>
2021-10-17 22:08:24 -0400

    one concat

--- contracts/Invite1155.sol
+++ contracts/Invite1155.sol
@@ -162,33 +162,17 @@ contract Invite1155 is ERC1155, Ownable, Whitelistable {
     function strConcat(
         string memory _a,
         string memory _b,
-        string memory _c,
-        string memory _d,
-        string memory _e
+        string memory _c
     ) internal pure returns (string memory) {
         bytes memory _ba = bytes(_a);
         bytes memory _bb = bytes(_b);
         bytes memory _bc = bytes(_c);
-        bytes memory _bd = bytes(_d);
-        bytes memory _be = bytes(_e);
-        string memory abcde = new string(
-            _ba.length + _bb.length + _bc.length + _bd.length + _be.length
-        );
+        string memory abcde = new string(_ba.length + _bb.length + _bc.length);
         bytes memory babcde = bytes(abcde);
         uint256 k = 0;
         for (uint256 i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
         for (uint256 i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
         for (uint256 i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-        for (uint256 i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-        for (uint256 i = 0; i < _be.length; i++) babcde[k++] = _be[i];
         return string(babcde);
-    }
-
-    function strConcat(
-        string memory _a,
-        string memory _b,
-        string memory _c
-    ) internal pure returns (string memory) {
-        return strConcat(_a, _b, _c, "", "");
     }
 }
