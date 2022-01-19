694fd7dcd7784529bcfbeb24b691ce7d7e2bb5fc
bennycio <benny@bennyc.io>
2022-01-19 13:37:14 -0500

    snapshot without calling contract

--- scripts/general/snapshot.js
+++ scripts/general/snapshot.js
@@ -1,40 +1,10 @@
 import fetch from "node-fetch"
 import fs from "fs"
-import { BigNumber, ethers } from "ethers"
 
 const openseaAPIKey = process.env.OPENSEA_API_KEY
 const openseaAPIURL = "https://api.opensea.io/api/v1/assets"
 const zeroAddress = "0x0000000000000000000000000000000000000000"
 const contractaddress = "0x13aAe6f9599880edbB7d144BB13F1212CeE99533"
-
-const provider = new ethers.providers.AlchemyProvider(
-  null,
-  process.env.ALCHEMY_API_KEY
-)
-
-const abi = [
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "tokenId",
-        type: "uint256",
-      },
-    ],
-    name: "tokenIdToProjectId",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-]
-
-const contract = new ethers.Contract(contractaddress, abi, provider)
 
 function sleep(ms) {
   return new Promise(resolve => {
@@ -42,17 +12,9 @@ function sleep(ms) {
   })
 }
 
-function dedupArray(arr) {
-  let seen = {}
-  return arr.filter(function (item) {
-    return seen.hasOwnProperty(item) ? false : (seen[item] = true)
-  })
-}
-
 const main = async () => {
   let snapshot = await fetchAssets(contractaddress, 0, 0)
 
-  console.log(snapshot)
   fs.writeFileSync("./snapshot.json", JSON.stringify(snapshot))
   console.log("Done!")
 }
@@ -86,9 +48,8 @@ const fetchAssets = async (contractAddress, offset, retry) => {
     if (address === zeroAddress) {
       continue
     }
-    let tid = BigNumber.from(asset.token_id)
-    let projectID = await contract.tokenIdToProjectId(tid, {})
-    if (projectID.toNumber() !== 1) {
+    let tid = parseInt(asset.token_id)
+    if (tid < 1000000 || tid > 1000887) {
       continue
     }
     toSnapshot.push(address)
