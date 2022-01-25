60735084c980f8cde2505142ce9c40bb92aa35a4
bennycio <benny@bennyc.io>
2022-01-25 12:24:44 -0500

    snapshot new

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,7 +29,7 @@ module.exports = {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
       from: PUBLIC_KEY,
-      gasPrice: 160000000000,
+      gasPrice: 152000000000,
     },
     test: {
       url: TEST_URL,
--- scripts/general/snapshot.js
+++ scripts/general/snapshot.js
@@ -1,50 +1,34 @@
 import fetch from "node-fetch"
 import fs from "fs"
-import { ethers } from "ethers"
+// import { ethers } from "ethers"
 
-const alchemyKey = process.env.ALCHEMY_API_KEY
+// const alchemyKey = process.env.ALCHEMY_API_KEY
 const openseaAPIKey = process.env.OPENSEA_API_KEY
 const openseaAPIURL = "https://api.opensea.io/api/v1/assets"
 const zeroAddress = "0x0000000000000000000000000000000000000000"
-const contractaddress = "0x13aAe6f9599880edbB7d144BB13F1212CeE99533"
-
-const provider = new ethers.providers.AlchemyProvider(null, alchemyKey)
-
-const abi = [
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-    ],
-    name: "tokensOfOwner",
-    outputs: [
-      {
-        internalType: "uint256[]",
-        name: "",
-        type: "uint256[]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
+const contractaddresses = [
+  "0x364547ADfc7a180744D762056Df35eeaEa803EC4",
+  "0x123214ef2bB526D1b3FB84A6D448985F537d9763",
+  "0xB228D7B6e099618Ca71bd5522B3a8c3788A8F172",
 ]
 
-const contract = new ethers.Contract(contractaddress, abi, provider)
-
 const main = async () => {
-  let snapshot = await fetchAssets(contractaddress, 0, 0)
+  let toSnapshot = []
+  for (let address of contractaddresses) {
+    console.log(`Fetching assets for ${address}`)
+    let snapshot = await fetchAssets(address, 0, 0)
+    toSnapshot = toSnapshot.concat(snapshot)
+  }
 
-  fs.writeFileSync("./snapshot.json", JSON.stringify(snapshot))
+  fs.writeFileSync("./snapshot_new.json", JSON.stringify(toSnapshot))
   console.log("Done!")
 }
 
 const fetchAssets = async (contractAddress, offset, retry) => {
   let toSnapshot = []
 
-  console.log("Fetching opensea...")
+  console.log(`Fetching assets for ${contractAddress}`)
+
   const res = await fetch(
     `${openseaAPIURL}?asset_contract_address=${contractAddress}&limit=50&offset=${offset}&order_direction=desc`,
     {
@@ -57,7 +41,7 @@ const fetchAssets = async (contractAddress, offset, retry) => {
   if (!res.ok) {
     if (res.status === 429 && retry < 3) {
       console.log("Too many requests, sleeping for a bit...")
-      await sleep(5000)
+      await sleep(5000 + retry * 1000)
       return fetchAssets(contractAddress, offset, retry + 1)
     }
     const text = await res.text()
@@ -72,10 +56,6 @@ const fetchAssets = async (contractAddress, offset, retry) => {
     if (address === zeroAddress) {
       continue
     }
-    let tid = parseInt(asset.token_id)
-    if (tid < 1000000 || tid > 1000887) {
-      continue
-    }
     toSnapshot.push(address)
   }
   if (assets.length === 50) {
@@ -84,22 +64,6 @@ const fetchAssets = async (contractAddress, offset, retry) => {
   }
 
   return dedupArray(toSnapshot)
-}
-
-const addUpLegendas = async owners => {
-  let total = 0
-  for (const owner of owners) {
-    let tokens = await contract.tokensOfOwner(owner)
-    for (const token of tokens) {
-      console.log(`${owner} - ${token.toNumber()}`)
-      let num = token.toNumber()
-      if (num < 1000000 || num > 1000887) {
-        continue
-      }
-      total++
-    }
-  }
-  return total
 }
 
 function sleep(ms) {
